94b353d0957755d3ecd0453f1fdc8e99146d6e46
Jessica Pavone <JessicaPavone24@yahoo.com>
2019-12-10 13:47:40 -0800

    adding and styling homepage

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 var express = require("express")
 var app = express()
 var port = 8080
-var path = require("path")
+var path = require("path")  
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static(__dirname + "/public"));
 
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname,  'public/index.html'));
+});
+
+app.get("/practice", function(req, res) {
+  res.sendFile(path.join(__dirname,  'public/practice.html'));
+});
 
 
 app.listen(process.env.PORT || 8080, function(){
     console.log("server running on port : " + port)
   });
-
+   
 
