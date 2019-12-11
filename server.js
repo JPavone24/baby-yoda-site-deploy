ac6dd2410f95f6a7a48f5701f07dbdec6b15734d
Jessica Pavone <JessicaPavone24@yahoo.com>
2019-12-11 15:42:16 -0800

    err

--- server.js
+++ server.js
@@ -8,24 +8,10 @@ app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
    
-
-
 // // serving routes
 var htmlroutes = require('./routes/htmlroutes');
 
 app.use('/', htmlroutes);
-
-
-var mongoose = require("mongoose")
-require('dotenv').config();
-const uri = process.env.ATLAS_URI;
-
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true ,  useUnifiedTopology: true }
-);
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
-})
 
 //deployment
 app.listen(process.env.PORT || 8080, function(){
